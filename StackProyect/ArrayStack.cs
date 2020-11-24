dc270411ef235568de615114bddcb758a884ba36
Francisco Perez Villalobos <57045338+Fran804@users.noreply.github.com>
2020-11-24 11:28:40 -0600

    Add files via upload

---
+++ StackProyect/ArrayStack.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTSLib
+{
+    class ArrayStack<T> : IStack<T>
+    {
+        private T[] data;
+        private int minCapacity;
+
+        public int Size { get; private set; }
+
+        public ArrayStack(int capacity)
+        {
+            data = new T[capacity];
+            minCapacity = capacity;
+            Size = 0;
+        }
+
+        public bool IsEmpty() => Size == 0;
+
+        public void Push(T element)
+        {
+            if (Size == data.Length)
+            {
+                T[] auxData = new T[data.Length * 2]; //crecer x2
+                Array.Copy(data,auxData, data.Length);
+                data = auxData;
+            }
+
+            data[Size++] = element;
+        }
+
+        public void Push(params T[] elements)
+        {
+            foreach (T e in elements)
+            {
+                Push(e);
+                //data[Size++] = e;
+            }
+        }
+
+        public T Pop()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("empty stack");
+            }
+            if (((Size-1) <= data.Length/10) && ((data.Length/2)>= minCapacity))
+            {
+                Array.Resize(ref data, data.Length / 2);
+            }
+
+            return data[--Size];
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("empty stack");
+            }
+
+            return data[Size - 1];
+        }
+
+        public override string ToString()
+        {
+            string debug = $"[C:{data.Length} S:{Size} Empty:{IsEmpty()}]";
+
+            debug += " { ";
+            for (int i = 0; i < Size; ++i)
+            {
+                debug += $"{data[i]} ";
+            }
+            debug += "}";
+
+            return debug;
+        }
+    }
+}
---
+++ StackProyect/Program.cs
@@ -0,0 +1,53 @@
+﻿using DTSLib;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackProyect
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ArrayStack<int> stack = new ArrayStack<int>(6);
+            Console.WriteLine(stack);
+
+            stack.Push(3);
+            Console.WriteLine(stack);
+
+            stack.Push(-2);
+            stack.Push(7);
+            Console.WriteLine(stack);
+
+            Console.WriteLine(stack.Peek());
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack);
+
+            //stack.Push(new int[] { 32, 1, 8, -4 });
+            stack.Push(32, 1, 8, -4);
+            Console.WriteLine(stack);
+
+            stack.Push(90);
+            Console.WriteLine(stack);
+
+
+            int num1 = stack.Pop();
+            stack.Push(num1);
+            stack.Push(num1);
+            Console.WriteLine(stack);
+
+            int largoStack = stack.Size;
+            for (int i = 0; i < largoStack; i++)
+            {
+                stack.Pop();
+            }
+            Console.WriteLine(stack);
+
+        }
+    }
+}
